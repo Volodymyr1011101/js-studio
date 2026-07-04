c7c1ab1c741ed5162a97e24f25cbcfc2c0e78a03
Volodymyr1011101 <voronkinvladimir328@egmail.com>
2026-07-04 20:44:42 +0200

    .

--- src/app/pages/kontakt/kontakt.ts
+++ src/app/pages/kontakt/kontakt.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import {ParticleEffectComponent} from "@app/components/particle-effect/particle-effect";
 import { KontaktComponent } from '@app/components/kontakt-component/kontakt';
+import {DynamicRenderItem} from '@app/components/dynamic-render-item/dynamic-render-item';
+import {Map} from '@app/components/map/map';
+import {TranslatePipe} from '@ngx-translate/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-kontakt-component-page',
   imports: [
-    KontaktComponent
+    KontaktComponent,
+    Map,
+    TranslatePipe,
+    NgOptimizedImage
   ],
   templateUrl: './kontakt.html',
   styleUrl: './kontakt.scss',
